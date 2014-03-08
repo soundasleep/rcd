eb9a7561b8065560ed380d1ba621dc6678590367
Jevon Wright <jevon@jevon.org>
2014-03-08 15:40:10 +1300

    Allowing players to be moved with WASD.

--- game.rb
+++ game.rb
@@ -8,9 +8,6 @@ srand
 
 # create a "map"
 class Map
-  @width = 20
-  @height = 20
-
   def initialize
     @data = Array.new(width * height, 0)
   end
@@ -26,14 +23,35 @@ class Map
   end
 end
 
-map = Map.new()
+class Player
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
 
-for i in 0..4
+  def x
+    @x
+  end
+  def y
+    @y
+  end
+  def x=(a)
+    @x=a
+  end
+  def y=(a)
+    @y=a
+  end
+end
+
+map = Map.new()
+player = Player.new(10, 5)
+
+for i in 0..25
   x = (rand(map.height) * map.width) + rand(map.width)
   map.data[x] = 1;
 end
 
-def draw_map(map)
+def draw_map(map, player)
   for y in 0..map.height
     setpos 2 + y, 2
     for x in 0..map.width
@@ -45,10 +63,35 @@ def draw_map(map)
       end
     end
   end
+
+  # find the player
+  setpos 2 + player.y, 2 + player.x
+  addstr "@"
   refresh
 end
 
 # s = stdscr
 
-draw_map(map)
-sleep 1
+Thread.new(map, player) {
+  loop do
+    draw_map(map, player)
+    sleep 0.05
+  end
+}
+
+# input thread
+# since ruby chomps up errors in a separate thread, we keep this on the mainthread
+loop do
+  case getch
+  when "Q"
+    exit
+  when "w"
+    player.y -= 1 unless player.y <= 0
+  when "s"
+    player.y += 1 unless player.y >= map.height
+  when "a"
+    player.x -= 1 unless player.x <= 0
+  when "d"
+    player.x += 1 unless player.x >= map.width
+  end
+end
